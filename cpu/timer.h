e8bb0b49ce34f2802ba00fe518d7c92ec4662cbc
520Matches <HengDi_Sun@163.com>
2020-02-12 16:27:58 +0800

    add timer and keyboard

---
+++ cpu/timer.h
@@ -0,0 +1,8 @@
+#ifndef _TIMER_H
+#define _TIMER_H
+
+#include "../kernel/util.h"
+
+void init_timer(u32 freq);
+
+#endif
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,14 +2,21 @@
 #include "util.h"
 #include "../cpu/isr.h"
 #include "../cpu/idt.h"
+#include "../cpu/timer.h"
+#include "../drivers/keyboard.h"
 
 void main()
 {
 //	clear_screen();
 	isr_install();
 
+	asm volatile("sti");
+//	init_timer(50);
+	init_keyboard();
+	/*
 	__asm__ __volatile__("int $2");
 	__asm__ __volatile__("int $3");
 	__asm__ __volatile__("int $33");
 	__asm__ __volatile__("int $42");
+	*/
 }
